322de6f774c72d41cd5dbadfe4bfa58222ed9a3c
Aniket <aniketdas810144@gmail.com>
2026-04-01 10:26:12 +0530

    intentite the dbconnect

--- src/app.js
+++ src/app.js
@@ -2,10 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const app = express();
+const authRouter = require("./routes/auth.route");
 
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
+
+
+app.use("/api/auth", authRouter);
 
 
 
